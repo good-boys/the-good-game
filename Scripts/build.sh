41a29f15f2153029d5089f5d659ff97308ff7640
Isaiah Mann <contact@isaiahmann.com>
2018-10-14 02:00:05 -0700

    Fix build script spacing

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,38 +3,37 @@
 project="GoodGame"
 
 echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity
-  -batchmode
-  -nographics
-  -silent-crashes
-  -logFile $(pwd)/unity.log
-  -projectPath $(pwd)
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe"
+/Applications/Unity_2018.2.11f1/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 
 echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity
-  -batchmode
-  -nographics
-  -silent-crashes
-  -logFile $(pwd)/unity.log
-  -projectPath $(pwd)
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app"
+/Applications/Unity_2018.2.11f1/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
 echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity
-  -batchmode
-  -nographics
-  -silent-crashes
-  -logFile $(pwd)/unity.log
-  -projectPath $(pwd)
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe"
+/Applications/Unity_2018.2.11f1/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
   -quit
 
 echo 'Logs from build'
 cat $(pwd)/unity.log
-
 
 echo 'Attempting to zip builds'
 zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
