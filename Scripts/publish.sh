5bc71af1d118d7451f68d4d5e23805d238288d05
Isaiah Mann <contact@isaiahmann.com>
2018-10-24 23:42:18 -0700

    Modify GitHub authentication

--- Scripts/publish.sh
+++ Scripts/publish.sh
@@ -2,9 +2,12 @@
 pushd ..
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
+git config --global credential.helper store
 git clone https://${GITHUB_TOKEN}github.com/good-boys/good-game-builds.git > /dev/null 2>&1
 cd good-game-builds
-cp ../the-good-game/Build .
+cp ../the-good-game/Build/*.zip .
+git rm origin
+git remote add origin https://good-boys:${GITHUB_TOKEN}@github.com/good-boys/good-game-builds.git
 git add --all
 git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
 git push
