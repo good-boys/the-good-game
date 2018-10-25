2c0a2bb134dc9db38bb467f9865d83b98ccd5d1d
Isaiah Mann <contact@isaiahmann.com>
2018-10-24 23:51:47 -0700

    Revise GitHub auth on push

--- Scripts/publish.sh
+++ Scripts/publish.sh
@@ -3,12 +3,10 @@ pushd ..
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
 git config --global credential.helper store
-git clone https://${GITHUB_TOKEN}github.com/good-boys/good-game-builds.git > /dev/null 2>&1
+git clone https://github.com/good-boys/good-game-builds.git
 cd good-game-builds
 cp ../the-good-game/Build/*.zip .
-git rm origin
-git remote add origin https://good-boys:${GITHUB_TOKEN}@github.com/good-boys/good-game-builds.git
 git add --all
 git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
-git push
+git push "https://${GITHUB_TOKEN}@github.com/good-boys/good-game-builds.git" master > /dev/null 2>&1
 popd
