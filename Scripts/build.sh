b113554201fc8748381d2028ddce8da3cf164df0
Isaiah Mann <isaiahbmann@gmail.com>
2018-10-28 11:00:15 -0700

    Enable Travis builds (#1) (#2)
    # Notes
    1. Travis downloads Unity + build extensions to host
    1. Travis creates builds for Windows, Linux, and Mac
    1. Travis creates a tag and pushes to GitHub: `<short Git hash>-<timestamp>`
    1. Travis uploads builds to the tag + creates a release
    1. Travis notifies Slack of build
    1. Created empty demo scene with "Hello World" on UI verify builds
    
    # Testing
    1. Verified on Travis build: https://travis-ci.com/good-boys/the-good-game/builds/89416049
    1. Release: https://github.com/good-boys/the-good-game/releases/tag/2de5993-2018-10-28-0904
    1. Downloaded and tested Mac build

---
+++ Scripts/build.sh
@@ -0,0 +1,45 @@
+#! /bin/sh
+
+project="GoodGame"
+
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -quit
+
+echo "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+  -quit
+
+echo "Attempting to build $project for Linux"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+  -quit
+
+echo 'Logs from build'
+cat $(pwd)/unity.log
+
+echo 'Attempting to zip builds'
+zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
+zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
+
+echo 'Renaming builds with timestamp + git hash'
+pushd Build
+popd
---
+++ Scripts/install.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+BASE_EDITOR_URL=https://netstorage.unity3d.com/unity
+BASE_INSTALLER_URL=http://download.unity3d.com/download_unity
+# HASH=38bd7dec5000
+# VERSION=2018.2.11f1
+HASH=6902ad48015d
+VERSION=2017.4.13f1
+
+download() {
+  base_url=$1
+  package=$2
+  url="$base_url/$HASH/$package"
+
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  base_url=$1
+  package=$2
+  download $base_url $package
+
+  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+install $BASE_EDITOR_URL "MacEditorInstaller/Unity-$VERSION.pkg"
+install $BASE_INSTALLER_URL "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install $BASE_INSTALLER_URL "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
