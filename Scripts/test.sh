af214b7fd9550cbcdcd55ae6926631133ecd722b
Isaiah Mann <contact@isaiahmann.com>
2018-10-28 11:36:24 -0700

    Enable proof of concept unit test

---
+++ Scripts/test.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+TEST_FILE=test.xml
+touch $TEST_FILE
+echo "Running Editor tests"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -runEditorTests \
+  -editorTestsResultFile $(pwd)/$TEST_FILE
+rc0=$?
+echo "Unit test results:"
+cat $(pwd)/test.xml
+# exit if tests failed
+echo ""
+if [ $rc0 -ne 0 ]; then { echo "===Unit tests failed==="; exit $rc0; } fi
+echo "===Unit tests passed==="
