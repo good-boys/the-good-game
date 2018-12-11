d2cb468f5b5b9c4675d39f27fb511d664984c082
Isaiah Mann <contact@isaiahmann.com>
2018-12-10 20:24:57 -0800

    Add damage test

--- Assets/Scripts/Test/Editor/Game/CharacterTest.cs
+++ Assets/Scripts/Test/Editor/Game/CharacterTest.cs
@@ -1,16 +1,43 @@
 ﻿using NUnit.Framework;
 
-public class CharacterTest 
+public class CharacterTest
 {
-	[Test]
+    Character character;
+
+    [SetUp]
+    public void Setup()
+    {
+        character = givenCharacter();
+    }
+
+
+    [Test]
 	public void TestInitCharacter() 
     {
         string testName = "test";
         int testHealth = 100;
-        Character character = new Character(testName, testHealth);
+        character = new Character(testName, testHealth);
 
         Assert.AreEqual(testName, character.Name);
         Assert.AreEqual(testHealth, character.Health);
         Assert.AreEqual(testHealth, character.MaxHealth);
+    }
+
+    [Test]
+    public void TestDamage()
+    {
+        int health = character.Health;
+        int damage = 5;
+
+        character.Damage(damage);
+
+        Assert.AreEqual(health - damage, character.Health);
+    }
+
+    Character givenCharacter()
+    {
+        string testName = "test";
+        int testHealth = 100;
+        return new Character(testName, testHealth);
     }
 }
