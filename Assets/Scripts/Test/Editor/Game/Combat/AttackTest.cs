27db4f752bcd5a002db3df2c7fe1c25e6d1236e7
Isaiah Mann <isaiah@truex.com>
2018-12-29 23:52:15 -0800

    Add additional tests

---
+++ Assets/Scripts/Test/Editor/Game/Combat/AttackTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+public class AttackTest
+{
+    [Test]
+    public void TestInit()
+    {
+        int damage = 5;
+
+        Attack attack = new Attack(null, damage, null, null);
+
+        Assert.AreEqual(damage, attack.Damage);
+    }
+}
---
+++ Assets/Scripts/Test/Editor/Game/Combat/CharacterActionTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Moq;
+
+public class CharacterActionTest
+{
+    [Test]
+    public void TestInit()
+    {
+        Character mockActor = new Mock<Character>("name", 100).Object;
+        Character mockTarget = new Mock<Character>("name", 100).Object;
+
+        CharacterAction characterAction = new CharacterAction(mockActor, null, mockTarget);
+
+        Assert.AreSame(mockActor, characterAction.Actor);
+        Assert.AreSame(mockTarget, characterAction.Targets[0]);
+    }
+
+}
