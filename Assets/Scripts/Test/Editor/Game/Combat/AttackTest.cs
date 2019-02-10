6af5e7d9350a427ac905774943112a3fe933325a
Isaiah Mann <contact@isaiahmann.com>
2019-02-10 12:52:34 -0800

    Terminate Travis on unit test failure

--- Assets/Scripts/Test/Editor/Game/Combat/AttackTest.cs
+++ Assets/Scripts/Test/Editor/Game/Combat/AttackTest.cs
@@ -11,4 +11,11 @@ public class AttackTest
 
         Assert.AreEqual(damage, attack.Damage);
     }
+
+    // TODO: Remove after verifying fail terminates Travis build
+    [Test]
+    public void AlwaysFail()
+    {
+        Assert.True(false);
+    }
 }
