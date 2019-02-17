080886d2b7a5e50a55cda48224544e68c751e1d2
Isaiah Mann <contact@isaiahmann.com>
2019-02-17 15:22:28 -0800

    Add AudioManager

---
+++ Assets/Scripts/Source/Audio/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour 
+{
+    Dictionary<string, AudioSource> sources = new Dictionary<string, AudioSource>();
+
+    public void RegisterAudioSource(AudioSource source)
+    {
+        sources[source.gameObject.name] = source;
+    }
+
+    public void Play(string sourceName, AudioClip clip)
+    {
+        AudioSource source;
+        if(!sources.TryGetValue(sourceName, out source))
+        {
+            Debug.LogError("Source {0} not found. Unable to play clip.");
+            return;
+        }
+
+        source.clip = clip;
+        source.Play();
+    }
+}
---
+++ Assets/Scripts/Source/Audio/TestAudioManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class TestAudioManager : MonoBehaviour 
+{
+    [SerializeField]
+    AudioSource musicSource;
+
+    [SerializeField]
+    AudioManager audioManager;
+
+    [SerializeField]
+    AudioClip music;
+
+	void Start()
+    {
+        audioManager.RegisterAudioSource(musicSource);
+        audioManager.Play("MusicSource", music);
+    }
+}
