31ac3574be2a6ddcda211c6218abc9e7818563dd
Isaiah Mann <isaiahbmann@gmail.com>
2019-03-03 14:43:20 -0800

    Add AudioManager (#8)
    * Create test scene for Audio
    * Add AudioManager
    * Add AudioManager unit tests
    * Create AudioOptions data object
    * Support clip fade transitions
    * Add access to playing sources and clips
    * Add test audio assets

---
+++ Assets/Scripts/Source/Audio/AudioManager.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour 
+{
+    Dictionary<string, AudioSource> sources = new Dictionary<string, AudioSource>();
+    Dictionary<string, AudioOptions> clipOptions = new Dictionary<string, AudioOptions>();
+    Dictionary<string, List<IEnumerator>> clipCoroutinesMap = new Dictionary<string, List<IEnumerator>>();
+
+    public void Init(Dictionary<string, AudioSource> sources)
+    {
+        this.sources = sources;
+    }
+
+    public void RegisterAudioSource(AudioSource source)
+    {
+        sources[source.gameObject.name] = source;
+    }
+
+    public void Play(string sourceName, AudioClip clip, AudioOptions options=null)
+    {
+        AudioSource source;
+        if(!sources.TryGetValue(sourceName, out source))
+        {
+            Debug.LogErrorFormat("Source {0} not found. Unable to play clip.", sourceName);
+            return;
+        }
+        if(options == null)
+        {
+            options = AudioOptions.GetDefault();
+        }
+        clipOptions[getClipIdentifier(source, clip)] = options;
+        handleClipTransition(source, clip, options);
+    }
+
+    public List<AudioSource> GetPlayingSources()
+    {
+        return sources.Values.Where(source => source.isPlaying).ToList();
+    }
+
+    public List<AudioClip> GetPlayingClips()
+    {
+        return GetPlayingSources().Select(source => source.clip).ToList();
+    }
+
+    void handleClipTransition(AudioSource source, AudioClip newClip, AudioOptions options)
+    {
+        AudioClip previousClip = source.clip;
+        string previousClipId = previousClip ? getClipIdentifier(source, previousClip) : null;
+        AudioOptions previousClipOptions;
+        if(!previousClip || !clipOptions.TryGetValue(previousClipId, out previousClipOptions))
+        {
+            previousClipOptions = AudioOptions.GetDefault();
+        }
+        IEnumerator fadeInCoroutine = null;
+        IEnumerator endClipCoroutine = null;
+        IEnumerator loopClipCoroutine = null;
+        Action endHandlerAction = delegate{ options.OnEnd(); };
+        if(options.HasFadeIn)
+        {
+            fadeInCoroutine = getFadeInCoroutine(source, options);
+        }
+        if(options.HasFadeOut)
+        {
+            IEnumerator fadeOutCoroutine = getFadeOutCoroutine(source, options, options.HasEndHandler ? endHandlerAction : null);
+            endClipCoroutine = waitAndExecute(Mathf.Max(0, newClip.length - options.FadeOutTime), delegate
+            {
+                StartCoroutine(fadeOutCoroutine);
+            });
+        }
+        else if(options.HasEndHandler)
+        {
+            endClipCoroutine = waitAndExecute(newClip.length, endHandlerAction);
+        }
+        if(options.Looping)
+        {
+            loopClipCoroutine = waitAndExecute(newClip.length, delegate
+            {
+                Play(source.name, newClip, options);
+            });
+        }
+        Action playClipAction = delegate
+        {
+            previousClipOptions.OnEnd();
+            source.clip = newClip;
+            if(fadeInCoroutine != null)
+            {
+                source.volume = 0;
+                StartCoroutine(fadeInCoroutine);
+                registerClipCoroutine(source, newClip, fadeInCoroutine);
+            }
+            if(endClipCoroutine != null)
+            {
+                StartCoroutine(endClipCoroutine);
+                registerClipCoroutine(source, newClip, endClipCoroutine);
+            }
+            if(loopClipCoroutine != null)
+            {
+                StartCoroutine(loopClipCoroutine);
+                registerClipCoroutine(source, newClip, loopClipCoroutine);
+            }
+            source.Play();
+        };
+        if(previousClip)
+        {
+            cleanUpClipCoroutines(source, previousClip);
+        }
+        if(options.HardTransitionIn || !previousClip || !previousClipOptions.HasFadeOut)
+        {
+            playClipAction();
+        }
+        else
+        {
+            IEnumerator fadeOutPrevious = getFadeOutCoroutine(source, previousClipOptions, playClipAction);
+            StartCoroutine(fadeOutPrevious);
+            registerClipCoroutine(source, newClip, fadeOutPrevious);
+        }
+    }
+
+    IEnumerator getFadeInCoroutine(AudioSource source, AudioOptions options, Action fadeEndHandler = null)
+    {
+        return getFadeCoroutine(source, options.Volume, options.FadeInTime, fadeEndHandler);
+    }
+
+    IEnumerator getFadeOutCoroutine(AudioSource source, AudioOptions options, Action fadeEndHandler = null)
+    {
+        return getFadeCoroutine(source, 0, options.FadeOutTime, fadeEndHandler);
+    }
+
+    IEnumerator getFadeCoroutine(AudioSource source, 
+                                 float endVolume, 
+                                 float time, 
+                                 Action fadeEndHandler = null)
+    {
+        float startVolume = source.volume;
+        float timer = 0;
+        while(timer <= time)
+        {
+            source.volume = Mathf.Lerp(startVolume, endVolume, timer / time);
+            timer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        if(fadeEndHandler != null)
+        {
+            fadeEndHandler();
+        }
+        source.volume = endVolume;
+    }
+
+    IEnumerator waitAndExecute(float waitTime, Action action)
+    {
+        yield return new WaitForSeconds(waitTime);
+        action();
+    }
+
+    void registerClipCoroutine(AudioSource source, 
+                               AudioClip clip, 
+                               IEnumerator coroutine)
+    {
+        List<IEnumerator> clipCoroutines;
+        string clipIdentifier = getClipIdentifier(source, clip);
+        if(!clipCoroutinesMap.TryGetValue(clipIdentifier, out clipCoroutines))
+        {
+            clipCoroutines = new List<IEnumerator>();
+            clipCoroutinesMap[clipIdentifier] = clipCoroutines;
+        }
+        clipCoroutines.Add(coroutine);
+    }
+
+    void cleanUpClipCoroutines(AudioSource source, AudioClip clip)
+    {
+        string clipIdentifier = getClipIdentifier(source, clip);
+        List<IEnumerator> clipCoroutines;
+        if(clipCoroutinesMap.TryGetValue(clipIdentifier, out clipCoroutines))
+        {
+            foreach(IEnumerator coroutine in clipCoroutines)
+            {
+                StopCoroutine(coroutine);
+            }
+            clipCoroutinesMap.Remove(clipIdentifier);
+        }
+    }
+
+    string getClipIdentifier(AudioSource source, AudioClip clip)
+    {
+        return string.Format("{0}{1}", source.name, clip.name);
+    }
+}
---
+++ Assets/Scripts/Test/Editor/Audio/AudioOptionsTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using NUnit.Framework;
+
+public class AudioOptionsTest
+{
+    [Test]
+    public void TestInit()
+    {
+        float fadeInTime = 0.3f;
+        float fadeOutTime = 0.0f;
+        float volume = 0.9f;
+        bool shouldLoop = true;
+        bool hardTransitionIn = false;
+        Action onEndHandler = delegate {};
+
+        AudioOptions options = new AudioOptions(
+            fadeInTime,
+            fadeOutTime,
+            volume,
+            shouldLoop,
+            hardTransitionIn,
+            onEndHandler);
+
+        Assert.AreEqual(fadeInTime, options.FadeInTime);
+        Assert.AreEqual(fadeOutTime, options.FadeOutTime);
+        Assert.AreEqual(volume, options.Volume);
+        Assert.AreEqual(shouldLoop, options.Looping);
+        Assert.AreEqual(hardTransitionIn, options.HardTransitionIn);
+        Assert.True(options.HasFadeIn);
+        Assert.False(options.HasFadeOut);
+        Assert.True(options.HasEndHandler);
+    }
+
+
+    [Test]
+    public void TestInit_ClampVolumeMin()
+    {
+        float fadeInTime = 0.3f;
+        float fadeOutTime = 0.0f;
+        float volume = -100f;
+        bool shouldLoop = true;
+        bool hardTransitionIn = false;
+        Action onEndHandler = delegate { };
+
+        AudioOptions options = new AudioOptions(
+            fadeInTime,
+            fadeOutTime,
+            volume,
+            shouldLoop,
+            hardTransitionIn,
+            onEndHandler);
+
+        Assert.AreEqual(0, options.Volume);
+    }
+
+    [Test]
+    public void TestInit_ClampVolumeMax()
+    {
+        float fadeInTime = 0.3f;
+        float fadeOutTime = 0.0f;
+        float volume = 100f;
+        bool shouldLoop = true;
+        bool hardTransitionIn = false;
+        Action onEndHandler = delegate { };
+
+        AudioOptions options = new AudioOptions(
+            fadeInTime,
+            fadeOutTime,
+            volume,
+            shouldLoop,
+            hardTransitionIn,
+            onEndHandler);
+
+        Assert.AreEqual(1f, options.Volume);
+    }
+
+    [Test]
+    public void TestOnEnd()
+    {
+        float fadeInTime = 0.3f;
+        float fadeOutTime = 0.0f;
+        float volume = 0.85f;
+        bool shouldLoop = true;
+        bool hardTransitionIn = false;
+        bool handlerRun = false;
+        Action onEndHandler = delegate 
+        {
+            handlerRun = true;
+        };
+        AudioOptions options = new AudioOptions(
+            fadeInTime,
+            fadeOutTime,
+            volume,
+            shouldLoop,
+            hardTransitionIn,
+            onEndHandler);
+
+        options.OnEnd();
+
+        Assert.True(handlerRun);
+    }
+
+    [Test]
+    public void TestGetDefault()
+    {
+        AudioOptions options = AudioOptions.GetDefault();
+
+        Assert.AreEqual(0, options.FadeInTime);
+        Assert.AreEqual(0, options.FadeOutTime);
+        Assert.False(options.Looping);
+        Assert.True(options.HardTransitionIn);
+        Assert.False(options.HasFadeIn);
+        Assert.False(options.HasFadeOut);
+        Assert.False(options.HasEndHandler);
+    }
+}
