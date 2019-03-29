1537a52cad448e4c76ceab43d8ec4d62c8f46f00
Isaiah Mann <contact@isaiahmann.com>
2019-03-29 01:27:58 -0700

    Basis of save system

---
+++ Assets/Scripts/Source/Save/GameSave.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+[Serializable]
+public class GameSave
+{
+    Random random;
+
+    public GameSave(int seed)
+    {
+        random = new Random(seed);
+    }
+}
---
+++ Assets/Scripts/Source/Save/SaveManager.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveManager
+{
+    string filePath;
+    BinaryFormatter binaryFormatter;
+
+    public SaveManager(string filePath)
+    {
+        this.filePath = filePath;
+        binaryFormatter = new BinaryFormatter();
+    }
+
+    public void Save(GameSave save)
+    {
+        FileStream file = File.OpenWrite(filePath);
+        binaryFormatter.Serialize(file, save);
+        file.Close();
+    }
+
+    public GameSave Load()
+    {
+        if(!File.Exists(filePath))
+        {
+            throw new FileNotFoundException();
+        }
+        FileStream file = File.OpenRead(filePath);
+        GameSave save = binaryFormatter.Deserialize(file) as GameSave;
+        file.Close();
+        return save;
+    }
+}
