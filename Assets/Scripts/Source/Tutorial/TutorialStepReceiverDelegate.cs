2decab636631fb93e782897bc92383e8bca9e132
Isaiah Mann <isaiahbmann@gmail.com>
2019-07-28 14:04:46 -0700

    [WIP] button highlight for attack tutorial

---
+++ Assets/Scripts/Source/Tutorial/TutorialStepReceiverDelegate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public enum TutorialStepReceiverDelegateAction
+{
+    Show,
+    Hide,
+}
+
+public class TutorialStepReceiverDelegate : MonoBehaviour 
+{
+    [SerializeField]
+    TutorialStepReceiverDelegateAction action;
+
+    private void Start()
+    {
+        setupReceiverAction();
+    }
+
+    private void setupReceiverAction()
+    {
+        TutorialStepReceiver receiver = GetComponent<TutorialStepReceiver>();
+        receiver.SetUp(getAction());
+    }
+
+    private Action getAction()
+    {
+        switch(action)
+        {
+            case TutorialStepReceiverDelegateAction.Show:
+                return () =>
+                {
+                    gameObject.SetActive(true);
+                };
+            case TutorialStepReceiverDelegateAction.Hide:
+                return () =>
+                {
+                    gameObject.SetActive(false);
+                };
+            default:
+                return null;
+        }
+    }
+}
